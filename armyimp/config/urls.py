c615c263569267685abe4d3b3591bb3c41bab3de
elbenfreund <elbenfreund@DenkenInEchtzeit.net>
2018-02-19 10:21:36 +0100

    Init

--- armyimp/config/urls.py
+++ armyimp/config/urls.py
@@ -8,3 +8,9 @@ urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
