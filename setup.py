1ab87f14724c323d687e5cae3cfaf3b95eed0d53
requires.io <support@requires.io>
2018-11-11 18:44:49 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ requires = [
     'django-nested-admin==3.1.2',
     'djangorestframework==3.9.0',
     'envdir==1.0.1',
-    'psycopg2-binary==2.7.6',
+    'psycopg2-binary==2.7.6.1',
     'pytz==2018.7',
     'rules==2.0',
 ]
