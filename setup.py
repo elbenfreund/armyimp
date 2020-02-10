48950ee32ad5e8c695376372fc9783856bcc291c
requires.io <support@requires.io>
2020-02-10 14:48:05 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requires = [
     'django-braces==1.14.0',
     'django-configurations==2.2',
     'django-crispy-forms==1.8.1',
-    'django-grappelli==2.13.3',
+    'django-grappelli==2.13.4',
     'django-model-utils==4.0.0',
     'django-nested-admin==3.2.4',
     'djangorestframework==3.11.0',
